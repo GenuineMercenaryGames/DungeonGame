7e4236bf63ecc00ab8a5261aced03abc7ba42cff
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-02-25 19:38:53 +0100

    Implemented basic logic for player controller

--- DungeonGame/Assets/Scripts/Controllers/PlayerController.cs
+++ DungeonGame/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float walkSpeed;
+
+    private CharacterController characterController;
+
+    private Vector2 inputMoveRaw;
+    private Vector2 inputMove;
+
+    private float delta;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        delta = Time.deltaTime;
+        UpdateMove();
+    }
+
+    public void InputMove(InputAction.CallbackContext ctx)
+    {
+        inputMoveRaw = ctx.ReadValue<Vector2>();
+        inputMove = inputMoveRaw.normalized;
+    }
+
+    private void UpdateMove()
+    {
+        // TODO : Change to use vectors relative to camera...
+        Vector3 move = (transform.forward * inputMove.y + transform.right * inputMove.x) * walkSpeed * delta;
+        characterController.Move(move);
     }
 }
