28f3decb87ac2b3f5be4aeaa48e192267be3798e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-02-28 17:29:44 +0100

    Added spin controller

---
+++ DungeonGame/Assets/Scripts/Utility/SpinController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SpinController : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] public Transform spinTarget;
+
+    [Header("Spin Settings")]
+    [SerializeField] public Vector3 spinAxis;
+    [SerializeField] public float spinSpeed;
+
+    void Start()
+    {
+        if(spinTarget == null)
+            spinTarget = transform;
+    }
+
+    void Update()
+    {
+        spinTarget.Rotate(spinAxis, spinSpeed * Time.deltaTime);
+    }
+}
