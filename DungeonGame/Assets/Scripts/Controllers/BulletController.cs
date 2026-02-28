f1b95f793294ead394c6a8939ad3dc99129bca27
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-02-28 03:15:41 +0100

    Implemented bullet controller basic logic

--- DungeonGame/Assets/Scripts/Controllers/BulletController.cs
+++ DungeonGame/Assets/Scripts/Controllers/BulletController.cs
@@ -2,15 +2,58 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region Variables
+
+    private Rigidbody rb;
+
+    [Header("Bullet Settings")]
+    [SerializeField] public float Damage;
+    [SerializeField] public float Speed;
+    [SerializeField] public float LifeTime;
+
+    private float elapsedTime;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Awake()
     {
-        
+        elapsedTime = 0.0f;
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        rb.linearVelocity = transform.forward * Speed;
+    }
+
     void Update()
     {
-        
+        if (elapsedTime >= LifeTime)
+        {
+            DestroyBullet();
+        }
+
+        elapsedTime += Time.deltaTime;
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // TODO : Implement other bullet impact logic. For now, just destroy the gameobject.
+        DestroyBullet();
+    }
+
+    #endregion
+
+    #region PrivateMethods
+
+    private void DestroyBullet()
+    {
+        // TODO : Implement object pooling logic later on. For now, we just destroy the bullet's gameobject and call it a day.
+        this.gameObject.SetActive(false);
+        Destroy(this.gameObject);
+    }
+
+    #endregion
 }
