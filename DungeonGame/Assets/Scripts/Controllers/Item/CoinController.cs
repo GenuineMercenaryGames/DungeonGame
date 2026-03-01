b4ed437178d7c15f4b93d8e8a2a3a01675713370
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-03-01 18:04:04 +0100

    Added logic to pick up coins and coint coin value

--- DungeonGame/Assets/Scripts/Controllers/Item/CoinController.cs
+++ DungeonGame/Assets/Scripts/Controllers/Item/CoinController.cs
@@ -1,15 +1,51 @@
 using UnityEngine;
 
+public static class CoinTypeData
+{
+    public enum CoinType
+    {
+        Bronze = 0,
+        Silver,
+        Gold,
+        Blue,
+        Purple,
+    }
+
+    public static int[] CoinValue = {
+        2,
+        10,
+        100,
+        1000,
+        2500
+    };
+
+    public static int GetCoinValue(int index)
+    {
+        if (index < 0 || index >= CoinValue.Length)
+            return 0;
+        return CoinValue[index];
+    }
+
+    public static int GetCoinValue(CoinType type)
+    {
+        return GetCoinValue((int)type);
+    }
+}
+
 // NOTE : Maybe I should implement a generic pickup-able item interface of sorts? or some sort of inheritance bullshit.
 // Not sure, I believe that this is good as it is, with a different component for each item type, or maybe this being configurable or whatever, but who knows.
 // We'll see what's best later on.
 public class CoinController : MonoBehaviour
 {
-    [SerializeField] private int value;
+    [SerializeField] private CoinTypeData.CoinType coinType;
 
     void OnTriggerEnter(Collider other)
     {
-        // TODO : Implement pickup logic.
+        if (other.TryGetComponent<PlayerController>(out var player))
+        {
+            player.Coins.Value += CoinTypeData.GetCoinValue(coinType);
+            DestroyCoin();
+        }
     }
 
     private void DestroyCoin()
