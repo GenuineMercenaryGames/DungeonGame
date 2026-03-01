5135125394ec31be1da9f4532752b397502f31fc
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-03-01 08:45:03 +0100

    Created health controller script

---
+++ DungeonGame/Assets/Scripts/Controllers/Health.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float currentValue;
+    [SerializeField] private float maxValue;
+    [SerializeField] private float minValue;
+
+    public float Value { get { return currentValue; } set { currentValue = value; } }
+    public float MinValue { get { return minValue; } set { minValue = value; } }
+    public float MaxValue { get { return maxValue; } set { maxValue = value; } }
+
+}
