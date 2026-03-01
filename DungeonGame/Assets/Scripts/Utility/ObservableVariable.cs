697699be7cda8880734819d25b1c0cdc733e67a7
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-03-01 09:04:42 +0100

    Implemented observable variable

---
+++ DungeonGame/Assets/Scripts/Utility/ObservableVariable.cs
@@ -0,0 +1,59 @@
+using System;
+
+public class ObservableVariable<T>
+{
+    
+    private T _value;
+    public Action<T, T> OnValueChanged;
+
+    public T Value { get { return GetValue(); } set { SetValueAndNotify(value); } }
+
+    public T ValueRaw { get { return GetValue(); } set { SetValueWithoutNotify(value); } }
+
+    public ObservableVariable(T value = default)
+    {
+        this._value = value;
+        this.OnValueChanged = null;
+    }
+
+    public void Notify(T oldValue, T newValue)
+    {
+        this.OnValueChanged?.Invoke(oldValue, newValue);
+    }
+
+    public void Notify()
+    {
+        Notify(_value, _value);
+    }
+
+    public void SetValue(T value, bool notify = true)
+    {
+        if (notify)
+        {
+            SetValueAndNotify(value);
+        }
+        else
+        {
+            SetValueWithoutNotify(value);
+        }
+    }
+
+    public void SetValueAndNotify(T value)
+    {
+        var oldValue = _value;
+        var newValue = value;
+        _value = newValue;
+        Notify(oldValue, newValue);
+    }
+
+    public void SetValueWithoutNotify(T value)
+    {
+        _value = value;
+    }
+
+    public T GetValue()
+    {
+        return _value;
+    }
+
+}
