585f23793ed5db4fc7e6d69f077518cfcf58c26f
Iago Caldentey Palmer <icaldenteypalmer@gmail.com>
2026-03-05 22:46:59 +0100

    Added demo scene for prototype and nature asset pack

---
+++ DungeonGame/Assets/Scripts/Generation/RoomDecorator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class RoomDecorator : MonoBehaviour
+{
+    [Tooltip("Possible prefabs to spawn")]
+    public GameObject[] decorationPrefabs;
+
+    [Tooltip("Planes to decorate")]
+    public GameObject[] roomPlanes;
+
+    [Tooltip("Number of objects per 100 square units")]
+    public float objectDensity = 5f;
+
+    public void DecoratePlanes()
+    {
+        foreach (GameObject plane in roomPlanes)
+        {
+            Vector3 planePos = plane.transform.position;
+            Vector3 planeScale = plane.transform.localScale;
+
+            float planeWidth = 10f * planeScale.x;
+            float planeLength = 10f * planeScale.z;
+
+            float planeArea = planeWidth * planeLength;
+            int objectsToSpawn = Mathf.RoundToInt(planeArea * objectDensity / 100f);
+
+            for (int i = 0; i < objectsToSpawn; i++)
+            {
+                float randomX = Random.Range(-planeWidth / 2f, planeWidth / 2f);
+                float randomZ = Random.Range(-planeLength / 2f, planeLength / 2f);
+
+                Vector3 spawnPos =
+                    planePos +
+                    plane.transform.right * randomX +
+                    plane.transform.forward * randomZ;
+
+                GameObject prefab = decorationPrefabs[Random.Range(0, decorationPrefabs.Length)];
+
+                Quaternion rot = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+
+                Instantiate(prefab, spawnPos, rot);
+            }
+            Destroy(plane);
+        }
+    }
+
+    void Start()
+    {
+        DecoratePlanes();
+    }
+}
---
+++ DungeonGame/Assets/Scripts/Generation/TreeSpawner.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour
+{
+    public GameObject treePrefab;
+    public GameObject[] treeSpawnAreas;
+
+    [Tooltip("Number of trees per 100 square units of plane")]
+    public float treeDensity = 5f;
+
+    public void SpawnTrees()
+    {
+        foreach (GameObject plane in treeSpawnAreas)
+        {
+            Vector3 planePos = plane.transform.position;
+            Vector3 planeScale = plane.transform.localScale;
+
+            float planeWidth = 10f * planeScale.x;
+            float planeLength = 10f * planeScale.z;
+
+            float planeArea = planeWidth * planeLength;
+            int treesToSpawn = Mathf.RoundToInt(planeArea * treeDensity / 100f);
+
+            for (int i = 0; i < treesToSpawn; i++)
+            {
+                float randomX = Random.Range(-planeWidth / 2f, planeWidth / 2f);
+                float randomZ = Random.Range(-planeLength / 2f, planeLength / 2f);
+
+                Vector3 spawnPos =
+                    planePos +
+                    plane.transform.right * randomX +
+                    plane.transform.forward * randomZ;
+
+                Quaternion rot = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+
+                Instantiate(treePrefab, spawnPos, rot);
+            }
+
+            plane.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    void Start()
+    {
+        SpawnTrees();
+    }
+}
