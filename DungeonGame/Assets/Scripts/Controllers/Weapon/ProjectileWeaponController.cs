d2eb911274a384a5d261cd19eb47e0fc869cc87a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-03-21 08:32:56 +0100

    Implemented single projectile weapon controller

--- DungeonGame/Assets/Scripts/Controllers/Weapon/ProjectileWeaponController.cs
+++ DungeonGame/Assets/Scripts/Controllers/Weapon/ProjectileWeaponController.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 
-public class ProjectileWeaponController : MonoBehaviour
+[RequireComponent(typeof(WeaponController))]
+public class SingleProjectileWeapon : MonoBehaviour
 {
-    [SerializeField] private WeaponControllerTest weaponController;
-    [SerializeField] private Transform spawnTransform;
     [SerializeField] private GameObject projectilePrefab;
-
+    [SerializeField] private float timeBetweenShots;
+    [SerializeField] private float spreadAngle;
+    
+    private WeaponController weaponController;
     private float elapsedTime;
+    private ObjectPoolController pool;
     
     void Start()
     {
-        elapsedTime = weaponController.BaseTimeBetweenShots;
+        pool = ObjectPoolManager.Instance.GetObjectPool(projectilePrefab);
+        weaponController = GetComponent<WeaponController>();
+        elapsedTime = timeBetweenShots;
     }
 
     void Update()
@@ -18,15 +23,44 @@ public class ProjectileWeaponController : MonoBehaviour
         elapsedTime += Time.deltaTime;
     }
 
+    public bool CanShoot()
+    {
+        return elapsedTime >= timeBetweenShots;
+    }
+
+    private void SpawnProjectile()
+    {
+        var bullet = pool.Get<BulletController>();
+        bullet.transform.position = weaponController.bulletSpawnTransform.position;
+        bullet.transform.rotation = Quaternion.LookRotation(GetRandomSpreadDirection(weaponController.bulletSpawnTransform.forward, spreadAngle));
+        bullet.Init();
+    }
+
     public void Shoot()
     {
-        if (elapsedTime < weaponController.BaseTimeBetweenShots)
+        if (!CanShoot())
             return;
         elapsedTime = 0.0f;
+        SpawnProjectile();
+    }
 
-        var go = Instantiate(projectilePrefab);
-        go.transform.position = spawnTransform.position;
-        go.transform.rotation = spawnTransform.rotation;
-        // NOTE : Here, assign the projectile damage to whatever it is plus the base weapon damage.
+    public void Shoot(int count)
+    {
+        if (!CanShoot())
+            return;
+        elapsedTime = 0.0f;
+        for (int i = 0; i < count; ++i)
+            SpawnProjectile();
+    }
+
+    private Vector3 GetRandomSpreadDirection(Vector3 forward, float angle)
+    {
+        Vector2 randomCircle = Random.insideUnitCircle;
+        float spreadRadius = Mathf.Tan(angle * Mathf.Deg2Rad * 0.5f);
+        Vector3 spread = new Vector3(randomCircle.x * spreadRadius, randomCircle.y * spreadRadius, 1.0f);
+        spread = spread.normalized;
+        Quaternion rot = Quaternion.LookRotation(forward);
+        Vector3 direction = rot * spread;
+        return direction;
     }
 }
