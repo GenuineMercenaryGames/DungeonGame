b92f4b603173f90d7d9125904735ee034198e6df
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-03-21 08:49:23 +0100

    Modified pick up logic

--- DungeonGame/Assets/Scripts/Controllers/Item/WeaponPickUp.cs
+++ DungeonGame/Assets/Scripts/Controllers/Item/WeaponPickUp.cs
@@ -2,13 +2,29 @@ using UnityEngine;
 
 public class WeaponPickUp : MonoBehaviour
 {
-    
+    [SerializeField] private MeshRenderer meshRenderer;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<PlayerController>(out var player))
         {
-            Debug.Log("The player is here!");
+            SetIsSelected(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<PlayerController>(out var player))
+        {
+            SetIsSelected(false);
+        }
+    }
+
+    private void SetIsSelected(bool isSelected)
+    {
+        foreach (var mat in meshRenderer.materials)
+        {
+            mat.SetInt("_IsSelected", isSelected ?  1 : 0);
         }
     }
 }
