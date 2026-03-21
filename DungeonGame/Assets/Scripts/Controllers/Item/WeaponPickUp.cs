577ea5427491c2138d58f57fb2c349c32636702c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-03-21 09:14:02 +0100

    Finished implementing initial weapon pickup logic

--- DungeonGame/Assets/Scripts/Controllers/Item/WeaponPickUp.cs
+++ DungeonGame/Assets/Scripts/Controllers/Item/WeaponPickUp.cs
@@ -3,12 +3,28 @@ using UnityEngine;
 public class WeaponPickUp : MonoBehaviour
 {
     [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private GameObject weaponPrefab; // NOTE : In the future, this will be the weapon SO. Don't worry, I'll get to it tomorrow when I'm back. Don't touch this yet please, It's a WIP and I'm too tired by now to deal with someone breaking this.
+
+    public void PickUp(PlayerController player)
+    {
+        player.weapons[player.equipedWeaponIndex] = weaponPrefab;
+        player.EquipWeapon(weaponPrefab);
+        Destroy(gameObject);
+    }
+
+    public void SetIsInRadius(bool isSelected)
+    {
+        foreach (var mat in meshRenderer.materials)
+        {
+            mat.SetInt("_IsSelected", isSelected ?  1 : 0);
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<PlayerController>(out var player))
         {
-            SetIsSelected(true);
+            SetIsInRadius(true);
         }
     }
 
@@ -16,15 +32,7 @@ public class WeaponPickUp : MonoBehaviour
     {
         if (other.TryGetComponent<PlayerController>(out var player))
         {
-            SetIsSelected(false);
-        }
-    }
-
-    private void SetIsSelected(bool isSelected)
-    {
-        foreach (var mat in meshRenderer.materials)
-        {
-            mat.SetInt("_IsSelected", isSelected ?  1 : 0);
+            SetIsInRadius(false);
         }
     }
 }
