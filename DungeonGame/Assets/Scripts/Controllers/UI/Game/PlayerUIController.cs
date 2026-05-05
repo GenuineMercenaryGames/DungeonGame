54ffe13967369339cfc5740b19f35ba5bc08f3be
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-05-05 20:21:39 +0200

    Implemented logic to update player face portrait based on health

--- DungeonGame/Assets/Scripts/Controllers/UI/Game/PlayerUIController.cs
+++ DungeonGame/Assets/Scripts/Controllers/UI/Game/PlayerUIController.cs
@@ -4,16 +4,26 @@ using UnityEngine.UI;
 
 public class PlayerUIController : MonoBehaviour
 {
+    [Header("Config")]
     [SerializeField] private float lerpSpeed;
+
+    [Header("Component References")]
     [SerializeField] private Image imageHealth;
     [SerializeField] private TMP_Text textCoins;
     [SerializeField] private TMP_Text textAmmo;
+
+    [Header("Portrait")]
+    [SerializeField] private Image[] facePortraitStates;
 
     void Start()
     {
         var player = PlayerManager.Instance.Player;
         player.Coins.AddListener(UpdateCoins);
         player.Coins.Notify();
+        player.healthController.Health.AddListener(UpdatePortrait);
+        player.healthController.MaxHealth.AddListener(UpdatePortrait);
+        player.healthController.Health.Notify();
+        player.healthController.MaxHealth.Notify();
     }
 
     void Update()
@@ -23,13 +33,27 @@ public class PlayerUIController : MonoBehaviour
 
     private void UpdateHealth()
     {
-        float health = PlayerManager.Instance.Player.healthController.Health.Value;
-        float maxHealth = PlayerManager.Instance.Player.healthController.MaxHealth.Value;
+        float health = GetPlayerHealth();
+        float maxHealth = GetPlayerMaxHealth();
         imageHealth.fillAmount = Mathf.Lerp(imageHealth.fillAmount, health / maxHealth, Time.deltaTime * lerpSpeed);
+    }
+
+    private void UpdatePortrait()
+    {
+        float health = GetPlayerHealth();
+        float maxHealth = GetPlayerMaxHealth();
+        float fraction = health / maxHealth;
+        int index = (int)(fraction * facePortraitStates.Length);
+        for (int i = 0; i < facePortraitStates.Length; ++i)
+            facePortraitStates[i].enabled = false;
+        facePortraitStates[index].enabled = true;
     }
 
     private void UpdateCoins(int coins)
     {
         textCoins.text = $"{coins}";
     }
+
+    private float GetPlayerHealth() { return PlayerManager.Instance.Player.healthController.Health.Value; }
+    private float GetPlayerMaxHealth() { return PlayerManager.Instance.Player.healthController.MaxHealth.Value; }
 }
