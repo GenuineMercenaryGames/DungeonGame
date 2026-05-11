52e67d9fbcbe08d910df6d0993ef9c10e913d5c0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-05-11 23:11:05 +0200

    Created script to update player skin on start

---
+++ DungeonGame/Assets/Scripts/Controllers/Player/PlayerSkinController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerSkinController : MonoBehaviour
+{
+    [SerializeField] private Renderer rendererReference;
+
+    private Material instance;
+
+    void Start()
+    {
+        instance = new Material(rendererReference.sharedMaterial);
+        instance.SetColor("_Color", GameConfigManager.Instance.skinColor);
+    }
+}
