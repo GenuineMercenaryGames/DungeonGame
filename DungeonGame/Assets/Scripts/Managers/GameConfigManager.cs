192432e22581bfaf3ec80d6a32bfab875c7acc92
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2026-05-13 06:19:40 +0200

    Changed the default color for the sweeper hazmat suit
    Otherwise, it would be charcoal black if no config was chosen lol... which is what happens when loading a test scene from the editor, because user settings loading was not implemented properly yet :P
    To fix that, instead of that happening on the init scene, settings would need to be loaded from a static method that runs on initialize always no matter what the scene. Quality and other such settings would need to be barred during debug tho, for safety and testability, so fuck that noise for now. There just ain't enough time for that bs.

--- DungeonGame/Assets/Scripts/Managers/GameConfigManager.cs
+++ DungeonGame/Assets/Scripts/Managers/GameConfigManager.cs
@@ -4,7 +4,7 @@ public class GameConfigManager : SingletonPersistent<GameConfigManager>
 {
     public GameObject selectedWeaponPrimary;
     public GameObject selectedWeaponSecondary;
-    public Color skinColor;
+    public Color skinColor = new Color(255.0f / 255.0f, 178.0f / 255.0f, 0.0f / 255.0f, 1.0f); // Default Sweeper yellow hazmat color.
 
     public int selectedSkin;
 
